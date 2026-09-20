20dc0e24a773ec66aace6e1ddd68b0f295507c78
Param <param1999mangra@gmail.com>
2026-09-20 21:47:37 +0530

    20

---
+++ CollectionFramework/Demo.java
@@ -0,0 +1,20 @@
+package CollectionFramework;
+import  java.util.List;
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+         list.add(55);
+        list.set(0,2);
+        // list.add(3,5);
+       
+        System.out.println(list);
+     
+      
+          System.out.print(  list.get(0));
+         
+        
+    }
+    
+}
