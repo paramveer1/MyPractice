950430db9d966d19fb5aa5dfce18e67f50dd05b5
Param <param1999mangra@gmail.com>
2026-09-23 22:14:54 +0530

    23sep

--- CollectionFramework/Demo.java
+++ CollectionFramework/Demo.java
@@ -8,14 +8,6 @@ import  java.util.ListIterator;
 import  java.util.Stack;
 
 public class Demo {
-    public static void main(String[] args) {
-Stack stack = new Stack();
-stack.add(4);
-stack.addAll(Arrays.asList(8,6,3,5));
-stack.add(3,8);
-System.out.println(stack);
-stack.pop();
-stack.push(9);
 
 System.out.println(stack.peek());
 
