ecddf48f0dc679033b3c1578bed61332b85b3dbc
Param <param1999mangra@gmail.com>
2026-09-26 14:43:11 +0530

    26sep

--- CollectionFramework/Demo.java
+++ CollectionFramework/Demo.java
@@ -6,17 +6,44 @@ import  java.util.Iterator;
 import  java.util.Arrays;
 import  java.util.ListIterator;
 import  java.util.Stack;
+import  java.util.HashMap;
+import  java.util.Map;
+import  java.util.Collection;
+import java.util.Set;
+
 
 public class Demo {
 
-System.out.println(stack.peek());
+public static void main(String[] args) {
 
+    Map<Integer,String> map =  new HashMap<>();
+    map.put(4,"param");
+    map.put(2,"pushpa");
+    map.put(1,"shanu");
+    map.put(3,"Deepa");
+
+     Set<Map.Entry<Integer,String>> entries = map.entrySet();
+     for(Map.Entry<Integer,String> o :entries){
+        // System.out.print(o.getKey()+ " = "+o.getValue()+ "     ");
+
+     }
+     Iterator itr = entries.iterator();
+     while(itr.hasNext()){
+        System.out.println(itr.next());
+
+     }
+    //  System.out.println(map);
+
+
+   
+    
+}
 
 
 
        
     }
-}
+
       
        
 
